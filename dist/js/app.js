77af9009179b7b162fadbbd14d249aee047e2842
Tweber663 <tweber.1995@hotmail.com>
2024-09-18 21:46:52 +0200

    working on random

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,15 +1,39 @@
 "use strict";
 const form = document.querySelector('form');
 const input = document.querySelector('input');
-input.addEventListener('input', (e) => {
-    const event = e;
-    event.data === "," ? createTag() : null;
-});
+const wrapper = document.querySelector('.choiceTagsWrapper');
 const createTag = () => {
     const confirm = form[0];
     const inputLength = confirm.value.split(",").length - 2;
     const inputInfo = confirm.value.split(",").filter((e) => e !== "")[inputLength];
-    ;
-    const finalProcess = inputInfo.split("").filter((e) => e !== " ").join("");
+    const createdElement = document.createElement('div');
+    createdElement.innerText = inputInfo;
+    createdElement.classList.add('tag');
+    wrapper.appendChild(createdElement);
+};
+input.addEventListener('input', (e) => {
+    const event = e;
+    event.data === "," ? createTag() : null;
+    console.log(event.data);
+});
+form.addEventListener('submit', (e) => {
+    const tags = document.querySelectorAll('div .tag');
+    e.preventDefault();
+    let counter = 0;
+    const interval = setInterval(() => {
+        randomFilter(counter);
+        counter++;
+        if (tags.length <= counter)
+            clearInterval(interval);
+    }, 350);
+});
+const randomFilter = (counter) => {
+    const tags = document.querySelectorAll('div .tag');
+    const random = Math.floor((Math.random() * tags.length));
+    tags[random].classList.add('active');
+    console.log(random);
+    setTimeout(() => {
+        tags[random].classList.remove('active');
+    }, 200);
 };
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,46 @@
 const form = document.querySelector('form') as HTMLFormElement; 
 const input = document.querySelector('input') as HTMLInputElement; 
-
-
-
-input.addEventListener('input', (e) => {
-    const event = e as InputEvent; 
-    event.data === "," ? createTag() : null; 
-})
-
+const wrapper = document.querySelector('.choiceTagsWrapper') as HTMLDivElement; 
 
 const createTag = () => {
     const confirm = form[0] as HTMLInputElement; 
     const inputLength = confirm.value.split(",").length - 2; 
-    const inputInfo = confirm.value.split(",").filter((e) => e !== "")[inputLength]; ; 
-    const finalProcess = inputInfo.split("").filter((e) => e !== " ").join(""); 
-
-
+    const inputInfo = confirm.value.split(",").filter((e) => e !== "")[inputLength];
+    const createdElement = document.createElement('div'); 
+    createdElement.innerText = inputInfo; 
+    createdElement.classList.add('tag'); 
+    wrapper.appendChild(createdElement); 
 }
+
+input.addEventListener('input', (e) => {
+    const event = e as InputEvent; 
+    event.data === "," ? createTag() : null; 
+    console.log(event.data)
+})
+
+form.addEventListener('submit', (e: Event) => {
+    const tags = document.querySelectorAll('div .tag'); 
+    e.preventDefault();
+    let counter = 0
+    const interval = setInterval(() => {
+        randomFilter(counter); 
+        counter ++; 
+        if (tags.length <= counter) clearInterval(interval);
+    }, 350); 
+})
+
+const randomFilter = (counter: Number) => {
+    const tags = document.querySelectorAll('div .tag'); 
+    const random = Math.floor((Math.random() * tags.length)); 
+    tags[random].classList.add('active');
+    console.log(random)
+    setTimeout(() => {
+        tags[random].classList.remove('active');
+    }, 200)
+}
+
+
+
+
+
 

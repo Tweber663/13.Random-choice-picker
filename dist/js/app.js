842c4d07550c6ddf0909a3e10b2cd93fcae517a3
Tweber663 <tweber.1995@hotmail.com>
2024-09-18 19:49:13 +0200

    js & css added

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,15 @@
-import Stopwatch from './StopwatchWithResults.js';
-new Stopwatch(document.querySelector('.stopwatch'));
+"use strict";
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+input.addEventListener('input', (e) => {
+    const event = e;
+    event.data === "," ? createTag() : null;
+});
+const createTag = () => {
+    const confirm = form[0];
+    const inputLength = confirm.value.split(",").length - 2;
+    const inputInfo = confirm.value.split(",").filter((e) => e !== "")[inputLength];
+    ;
+    const finalProcess = inputInfo.split("").filter((e) => e !== " ").join("");
+};
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
-import Stopwatch from './StopwatchWithResults.js'
+const form = document.querySelector('form') as HTMLFormElement; 
+const input = document.querySelector('input') as HTMLInputElement; 
 
-new Stopwatch(document.querySelector('.stopwatch'))
+
+
+input.addEventListener('input', (e) => {
+    const event = e as InputEvent; 
+    event.data === "," ? createTag() : null; 
+})
+
+
+const createTag = () => {
+    const confirm = form[0] as HTMLInputElement; 
+    const inputLength = confirm.value.split(",").length - 2; 
+    const inputInfo = confirm.value.split(",").filter((e) => e !== "")[inputLength]; ; 
+    const finalProcess = inputInfo.split("").filter((e) => e !== " ").join(""); 
+
+
+}
+
